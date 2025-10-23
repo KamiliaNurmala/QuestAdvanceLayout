32adef80f6fd206830d00fc7ab07b0099a01f8fd
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-10-23 15:46:36 +0700

    tambah import dasar untuk layout Compose

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -1,1 +1,8 @@
 package com.example.praktikum3
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
